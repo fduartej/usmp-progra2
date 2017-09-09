d7e0987e271b131e09be7e5f46f2bdc316fc4ad5
Clase <Clase@1A-PC30>
2017-09-09 10:33:11 -0500

    Orgaize Impport

--- app/src/main/java/com/example/demo/control/AddPersonaController.java
+++ app/src/main/java/com/example/demo/control/AddPersonaController.java
@@ -9,8 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.model.Persona;
 import com.example.demo.model.PersonaRepository;
@@ -20,26 +18,26 @@ public class AddPersonaController {
 
 	@Autowired
 	private PersonaRepository personaRepo;
-	
+
 	@GetMapping("/addPerson")
 	public String loadFormPerson(Model model) {
 		model.addAttribute("persona", new Persona());
 		return "addPerson";
 	}
-	
+
 	@PostMapping("/addPerson")
 	public String submitPerson(@ModelAttribute Persona persona) {
-		
+
 		personaRepo.save(persona);
 
-		int total= persona.getEdad()+ persona.getIncremento();
+		int total = persona.getEdad() + persona.getIncremento();
 		persona.setIncremento(total);
 		return "result";
 	}
-	
+
 	@GetMapping("/listPerson")
-	public String list(Map<String,Object> model) {
-		List<Persona> pers=personaRepo.findAll();
+	public String list(Map<String, Object> model) {
+		List<Persona> pers = personaRepo.findAll();
 		model.put("pers", pers);
 		return "persList";
 	}
